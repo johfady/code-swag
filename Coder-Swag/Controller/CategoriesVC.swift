b0963c7d970df64853a29583d6371cb35caa4333
johfady <youssefq2007@yahoo.com>
2017-12-23 20:29:38 +0200

    initial

---
+++ Coder-Swag/Controller/CategoriesVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coder-Swag
+//
+//  Created by Youssef Wahba on 12/23/17.
+//  Copyright © 2017 Youssef Wahba. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
